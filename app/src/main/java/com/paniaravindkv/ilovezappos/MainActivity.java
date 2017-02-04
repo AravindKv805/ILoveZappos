5d3925aeb78430e090e51f9299bf89f75b631609
Pani Aravind K V <Aravind@panis-mbp.mobile.asu.edu>
2017-02-03 19:16:19 -0700

    basic button action

--- app/src/main/java/com/paniaravindkv/ilovezappos/MainActivity.java
+++ app/src/main/java/com/paniaravindkv/ilovezappos/MainActivity.java
@@ -1,14 +1,28 @@
 package com.paniaravindkv.ilovezappos;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    EditText searchEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchEditText = (EditText) findViewById(R.id.searchEditText);
+    }
+
+    public void searchAction(View view) {
+
+        String searchText = searchEditText.getText().toString();
+
+        Log.i("INFO", "Search Button Tapped");
+        Log.i("INFO", "Search Text - " + searchText);
     }
 }
