a174082b88a4370db60a246f6037cb2af8e492c6
Pani Aravind K V <Aravind@panis-mbp.mobile.asu.edu>
2017-02-03 19:51:01 -0700

    call API and log result

--- app/src/main/java/com/paniaravindkv/ilovezappos/MainActivity.java
+++ app/src/main/java/com/paniaravindkv/ilovezappos/MainActivity.java
@@ -5,10 +5,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by Pani Aravind on 2/3/17.
+ */
 
 public class MainActivity extends Activity {
 
     EditText searchEditText;
+    DownloadTask downloadTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +35,37 @@ public class MainActivity extends Activity {
 
         Log.i("INFO", "Search Button Tapped");
         Log.i("INFO", "Search Text - " + searchText);
+
+        downloadTask = new DownloadTask();
+
+        String baseUrl = getString(R.string.api_base_url);
+        String apiKey = getString(R.string.api_key);
+
+        String urlString = baseUrl + "?term=" + searchText + "&key=" + apiKey;
+
+        String result = "";
+
+        Log.i("INFO", "Search API URL - " + urlString);
+
+        try {
+            result = downloadTask.execute(urlString).get();
+
+            Log.i("INFO", "API call result - " + result);
+
+            JSONObject resultInJSON = new JSONObject(result);
+
+            Log.i("INFO", "API call result count - " + resultInJSON.getString("currentResultCount"));
+
+            if (resultInJSON.getString("currentResultCount").equals("0")) {
+
+                Toast.makeText(getApplicationContext(), "No results found!!", Toast.LENGTH_LONG).show();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
