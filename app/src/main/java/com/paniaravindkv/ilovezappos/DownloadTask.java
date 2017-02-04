f40c70ec9c50eff5376460ddfee33cb2ed184a76
Pani Aravind K V <Aravind@panis-mbp.mobile.asu.edu>
2017-02-03 19:50:35 -0700

    download tasker

---
+++ app/src/main/java/com/paniaravindkv/ilovezappos/DownloadTask.java
@@ -0,0 +1,58 @@
+package com.paniaravindkv.ilovezappos;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Pani Aravind on 2/3/17.
+ */
+
+public class DownloadTask extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... urls) {
+
+        String result = "";
+        URL url;
+        HttpURLConnection httpURLConnection = null;
+
+        try {
+            url = new URL(urls[0]);
+
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream in = httpURLConnection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(in);
+
+            int data = inputStreamReader.read();
+
+            while (data != -1) {
+                char current = (char) data;
+                result += current;
+                data = inputStreamReader.read();
+            }
+
+            return result;
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+
+    }
+}
