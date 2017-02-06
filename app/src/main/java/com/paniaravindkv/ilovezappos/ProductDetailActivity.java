627a4f4e598d210498d84f9eb3a56bb34ceadea4
Pani Aravind K V <Aravind@Panis-MacBook-Pro.local>
2017-02-05 19:45:51 -0700

    add to cart functionality and menu item

--- app/src/main/java/com/paniaravindkv/ilovezappos/ProductDetailActivity.java
+++ app/src/main/java/com/paniaravindkv/ilovezappos/ProductDetailActivity.java
@@ -3,8 +3,14 @@ package com.paniaravindkv.ilovezappos;
 import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
 import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -28,6 +34,8 @@ public class ProductDetailActivity extends AppCompatActivity {
     TextView originalPriceTextView;
     TextView priceTextView;
     TextView percentOffTextView;
+
+    private int cartCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,7 +80,6 @@ public class ProductDetailActivity extends AppCompatActivity {
                 originalPriceTextView.setPaintFlags(originalPriceTextView.getPaintFlags()| Paint.STRIKE_THRU_TEXT_FLAG);
             }
 
-
             binding.setProduct(product);
         } catch (JSONException e) {
             e.printStackTrace();
@@ -84,4 +91,50 @@ public class ProductDetailActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.cartItem);
+        menuItem.setIcon(buildCounterDrawable(cartCount, R.drawable.ic_cart_menuitem));
+
+        return true;
+    }
+
+    private Drawable buildCounterDrawable(int count, int backgroundImageId) {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.cart_menuitem_layout, null);
+        view.setBackgroundResource(backgroundImageId);
+
+        if (count == 0) {
+            View counterTextPanel = view.findViewById(R.id.cartMenuLayout);
+            counterTextPanel.setVisibility(View.GONE);
+        } else {
+            TextView textView = (TextView) view.findViewById(R.id.cartCounter);
+            textView.setText("" + count);
+        }
+
+        view.measure(
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+
+        view.setDrawingCacheEnabled(true);
+        view.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+        Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+        view.setDrawingCacheEnabled(false);
+
+        return new BitmapDrawable(getResources(), bitmap);
+    }
+
+    public void addToCart(View view) {
+
+        Log.i("INFO", "add to cart clicked");
+
+        if (cartCount == 0)
+            cartCount++;
+        invalidateOptionsMenu();
+    }
+
 }
+
